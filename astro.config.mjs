45f245c32c669d9ffdf3964a09dd9d780e2ae165
Amo Wu <amo260@gmail.com>
2026-05-09 22:40:09 +0800

    Revert "更新網站網址和基底路徑設定"
    This reverts commit 416ed5c80151310929da0c545e36572b34c6d0e5.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://amowu.github.io',
-	base: 'blog',
+	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
